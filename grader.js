b51cda636e26037dc8cc7e6a5e68fcc0700aa803
Ubuntu <ubuntu@ip-172-31-28-65.us-west-2.compute.internal>
2013-07-10 02:00:14 +0000

    Broken - complete task

--- grader.js
+++ grader.js
@@ -19,16 +19,50 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + restler
+ - https://github.com/danwrong/restler
+
 */
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://serene-meadow-2070.herokuapp.com";
+
+
+
+
+
+var buildfn = function() {
+    var response = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+	}
+    return response;
+};
+
+
+
+/*
+var assertValidUrl = function(url) {
+    var instr = url.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+*/
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
+    var response = buildfn();
+    rest.get(apiurl).on('complete', response);
+
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
@@ -36,13 +70,19 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
+
+
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
@@ -55,20 +95,32 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file_url>', 'URL to index.html')
         .parse(process.argv);
+
     var checkJson = checkHtmlFile(program.file, program.checks);
+
+
+    var response2console = buildfn(csvfile, headers);
+    rest.get(apiurl).on('complete', response2console);
+
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
